ff6b7b8b3b33a7bfe61c0fdaa3f68bd12802df07
Lee Dohm <lee@lee-dohm.com>
2016-10-18 18:14:49 -0700

    :art:

--- web/controllers/version_controller.ex
+++ web/controllers/version_controller.ex
@@ -7,24 +7,26 @@ defmodule AtomTools.VersionController do
 
   def show(conn, params) do
     version_info = params["version"]["info"]
-    {atom_version, electron_version} = parse(version_info)
+    versions = parse(version_info)
+    actual_version = get_electron_version_for(versions)
+
+    render conn, "show.html", atom_version: versions["Atom"], electron_version: versions["Electron"], actual_version: actual_version
+  end
+
+  defp get_electron_version_for(%{"Atom" => atom_version}) do
     %{body: body, status_code: 200} = HTTPotion.get("https://raw.githubusercontent.com/atom/atom/v#{atom_version}/package.json")
     package_info = Poison.decode!(body)
-    actual_version = package_info["electronVersion"]
-
-    render conn, "show.html", atom_version: atom_version, electron_version: electron_version, actual_version: actual_version
+    package_info["electronVersion"]
   end
 
   defp parse(info) do
-    versions = info
-               |> String.split("\n")
-               |> Enum.map(fn(text) -> String.split(text, ":") end)
-               |> Enum.reduce(%{}, fn(pair, map) ->
-                    key = String.trim(List.first(pair))
-                    value = String.trim(List.last(pair))
-                    Map.put(map, key, value)
-                  end)
-
-    {versions["Atom"], versions["Electron"]}
+    info
+    |> String.split("\n")
+    |> Enum.map(fn(text) -> String.split(text, ":") end)
+    |> Enum.reduce(%{}, fn(pair, map) ->
+         key = String.trim(List.first(pair))
+         value = String.trim(List.last(pair))
+         Map.put(map, key, value)
+       end)
   end
 end
